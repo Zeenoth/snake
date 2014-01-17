9897a38a1eb4b8b14a7bc99d733bbc49d3f77fb8
sophie <sophie.shiyi.francoise@gmail.com>
2014-01-17 17:24:30 +0100

    début fonction jeu

---
+++ controle.h
@@ -0,0 +1,29 @@
+#ifndef _CONTROLE_H_
+#define _CONTROLE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+
+#include <SDL/SDL_image.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_draw.h>
+#include <SDL/SDL_ttf.h>
+
+#include "liste.h"
+#include "fonctions.h"
+#include "init.h"
+#include "global.h"
+
+
+
+
+
+
+
+
+
+
+#endif
